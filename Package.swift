ff8aa6f7f62c2ce96ff552cbc6f01afb235f60d2
Isaac Greenspan <ilg@2718.us>
2024-04-02 15:09:25 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftDigest",
+    products: [
+        .library(name: "SwiftDigest", targets: ["SwiftDigest"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftDigest",
+            path: "SwiftDigest"
+        ),
+    ]
+)
